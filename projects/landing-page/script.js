602307de9fb416eccda238e53797c5d02e5b4f3b
Milianore <soniqnuacc@gmail.com>
2024-11-01 14:01:35 +0300

    create demo

---
+++ projects/landing-page/script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    let height1 = document.getElementsByClassName('intro__img')[0].clientHeight;
+    document.getElementsByClassName("intro__info")[0].style.height = height1 + 'px';
+
+};
